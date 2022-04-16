a591a7ae6ffa5ccab4ed9b378c310e1f506906b3
FabiolaEscobar <fabiola.escobar.monsalve@gmail.com>
2022-04-15 23:32:32 -0400

    Update IntroductionToPython.py

--- IntroductionToPython.py
+++ IntroductionToPython.py
@@ -87,5 +87,125 @@ areas_2 = areas_1 + ["garage",15.45]
 
 print(areas_1)
 print(areas_2)
-
 print(areas)
+
+##################################
+
+# Create variables var1 and var2
+var1 = [1, 2, 3, 4]
+var2 = True
+
+# Print out type of var1  function type
+print(type(var1))
+
+# Print out length of var1  function len
+print(len(var1))
+
+# Convert var2 to an integer: out2
+out2 = int(var2)
+print(out2)
+
+##################################
+
+# Function help: find description of function
+help(max)
+
+##################################
+
+# Create lists first and second
+first = [11.25, 18.0, 20.0]
+second = [10.75, 9.50]
+
+# Paste together first and second: full
+full = first + second
+
+# Sort full in descending order: full_sorted   functino sorted
+full_sorted = sorted(full, reverse = True)
+
+# Print out full_sorted
+print(full_sorted)
+
+##################################
+
+# string to experiment with: place
+place = "poolhouse"
+
+# Use upper() on place: place_up   function upper
+place_up = place.upper()
+
+# Print out place and place_up
+print(place)
+print(place_up)
+
+# Print out the number of o's in place      function count
+print(place.count("o"))   # answer is 3
+
+##################################
+
+# Create list areas
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Print out the index of the element 20.0   function index
+print(areas.index(20.0))  # answer 2
+
+# Print out how often 9.50 appears in areas
+print(areas.count(9.50))
+
+##################################
+
+# Create list areas
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Use append twice to add poolhouse and garage size     function append
+areas.append(24.5)
+areas.append(15.45)
+
+# Print out areas
+print(areas)
+
+# Reverse the orders of the elements in areas       function reverse
+areas.reverse()    # voltea la lista lo ultimo de primero y así sucesivamente
+
+# Print out areas
+print(areas)
+
+##################################
+
+# Definition of radius     
+r = 0.43
+
+# Import the math package    ALL PACKAGE
+import math 
+
+# Calculate C
+C = 2 * math.pi * r 
+
+# Calculate A
+A = math.pi * (r ** 2)
+
+# Build printout
+print("Circumference: " + str(C))    # function str
+print("Area: " + str(A))
+
+##################################
+
+# Definition of radius
+r = 192500
+
+# Import radians function of math package    ONE FUNCTION OF PACKAGE
+from math import radians
+
+# Travel distance of Moon over 12 degrees. Store in dist.
+dist = r * radians(12)
+
+# Print out dist
+print(dist)
+
+##################################
+
+# from PACKAGE.SUBPACKAGE import FUNCTION as ALIAS_FUNCTION
+from scipy.linalg import inv as my_inv
+
+
+
+
