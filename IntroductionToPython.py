9666ebcd9ec1ad8499d97841ea865254b94f2e01
FabiolaEscobar <fabiola.escobar.monsalve@gmail.com>
2022-04-15 23:03:34 -0400

    Update IntroductionToPython.py

--- IntroductionToPython.py
+++ IntroductionToPython.py
@@ -12,10 +12,12 @@ areas = ["hallway", hall, "kitchen", kit, "living room", liv, "bedroom", bed, "b
 print(areas)
 
 ##################################
+
 # Create areas_copy
 areas_copy = areas[:]
 
 ##################################
+
 # Create the areas list
 areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
 
@@ -29,6 +31,7 @@ print(areas[-1])
 print(areas[5])
 
 ##################################
+
 # Create the areas list
 areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
 
@@ -49,3 +52,40 @@ upstairs = areas[6:]
 # Print out downstairs and upstairs
 print(downstairs)
 print(upstairs)
+
+# Alternative slicing to create downstairs
+downstairs = areas[:6]
+
+# Alternative slicing to create upstairs
+upstairs = areas[6:]
+
+print(downstairs)
+print(upstairs)
+
+##################################
+
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Correct the bathroom area
+areas[-1] = 10.50
+
+# Change "living room" to "chill zone"
+areas[4] =  "chill zone"
+
+##################################
+
+# Create the areas list and make some changes
+areas = ["hallway", 11.25, "kitchen", 18.0, "chill zone", 20.0,
+         "bedroom", 10.75, "bathroom", 10.50]
+
+# Add poolhouse data to areas, new list is areas_1
+areas_1 = areas + ["poolhouse",24.5]
+
+# Add garage data to areas_1, new list is areas_2
+areas_2 = areas_1 + ["garage",15.45]
+
+print(areas_1)
+print(areas_2)
+
+print(areas)
