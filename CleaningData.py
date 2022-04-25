5ea6330412216b4d6f2ed5e2ec262fc5e239f08c
FabiolaEscobar <fabiola.escobar.monsalve@gmail.com>
2022-04-25 18:49:02 -0400

    Update CleaningData.py

--- CleaningData.py
+++ CleaningData.py
@@ -21,3 +21,32 @@ assert ride_sharing['user_type_cat'].dtype == 'category'
 
 # Print new summary statistics 
 print(ride_sharing['user_type_cat'].describe())
+
+################################
+
+# Strip duration of minutes
+ride_sharing['duration_trim'] = ride_sharing['duration'].str.strip('minutes')
+
+# Convert duration to integer
+ride_sharing['duration_time'] = ride_sharing['duration_trim'].astype('int')
+
+# Write an assert statement making sure of conversion
+assert ride_sharing['duration_time'].dtype == 'int'
+
+# Print formed columns and calculate average ride duration 
+print(ride_sharing[['duration','duration_trim','duration_time']])
+print(ride_sharing[['duration','duration_trim','duration_time']].mean())
+
+######################################
+
+# Convert tire_sizes to integer
+ride_sharing['tire_sizes'] = ride_sharing['tire_sizes'].astype('int')
+
+# Set all values above 27 to 27
+ride_sharing.loc[ride_sharing['tire_sizes'] > 27, ride_sharing['tire_sizes']] = 27
+
+# Reconvert tire_sizes back to categorical
+ride_sharing['tire_sizes'] = ride_sharing['tire_sizes'].astype('category')
+
+# Print tire size description
+print(ride_sharing['tire_sizes'].describe())
